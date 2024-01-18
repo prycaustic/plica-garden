3d9df4bb0fdb923626b3ce3aae9bce4b8963873e
Andrei Neacsu <andnea823@gmail.com>
2024-01-18 12:08:57 -0600

    woops fix that

--- app.js
+++ app.js
@@ -80,11 +80,11 @@ app.get('/:location', (req, res) => {
                 continue;
             }
 
+            if (file.startsWith('_assets')) continue;
             
             if (fs.lstatSync(fullPath).isDirectory()) {
                 let firstFile = fs.readdirSync(fullPath)[0];
                 if (firstFile.endsWith('.md')) continue;
-                if (firstFile.startsWith('_assets')) continue;
                 let previewPath = path.join(filePath, firstFile);
 
                 filesList += `\n<li>\n<a href="/view/${filePath}">`;
