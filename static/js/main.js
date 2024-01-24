0c24f2d6bc416cb375ee6d32dc4a1295108d5648
Andrei Neacsu <andnea823@gmail.com>
2024-01-24 11:32:29 -0600

    Move hidden folders toggle into a settings menu (still broken)

--- static/js/main.js
+++ static/js/main.js
@@ -1,40 +1,44 @@
 window.addEventListener('load', () => {
-    let nav = document.querySelector("nav");
+    let footer = document.querySelector("footer");
+    let menu = '';
 
-    nav.innerHTML += '<span id="toggle-dirs" class="icon material-symbols-sharp small dark" onclick="toggleHiddenDirs()" title="Enable hidden folders">visibility_off</span>';
+    // There's gotta be a better way to build this lol
+    menu += '\n<label id="settings-label" class="icon-checkbox" for="settings-menu-toggle"><i class="icon icon-gear dark"></i></label>';
+    menu += '\n<input id="settings-menu-toggle" type="checkbox" />';
+    menu += '\n<fieldset id="settings-menu" class="rounded shadow">';
+    menu += '\n<span class="legend">Settings</span>';
+    menu += '\n<label for="show-hidden-dirs">Show hidden folders</label>';
+    menu += '\n<input id="show-hidden-dirs" type="checkbox" />';
+    menu += '\n</fieldset>';
+    footer.innerHTML += menu;
     directories = document.querySelectorAll('.hidden-dir');
-    icon = document.querySelector('#toggle-dirs');
+    checkbox = document.querySelector('#show-hidden-dirs');
+
+    checkbox.addEventListener('change', () => {
+        // fucking javascript LMAO!
+        setHiddenDirs(checkbox.checked.toString());
+    });
     
     loadHiddenState();
 });
 
-function toggleHiddenDirs() {
-    directories.forEach((element) => {
-        if (element.classList.contains('hidden')) {
-            // Hidden dirs are VISIBLE
-            element.classList.remove('hidden');
-            localStorage.setItem('hiddenDirsVisible', 'true');
-            icon.innerText = 'visibility';
-            icon.title = 'Disable hidden folders';
-        }
-        else {
-            // Hidden dirs are NOT VISIBLE
-            element.classList.add('hidden');
-            localStorage.setItem('hiddenDirsVisible', 'false');
-            icon.innerText = 'visibility_off';
-            icon.title = 'Enable hidden folders';
-        }
-    });
-};
-
 function loadHiddenState() {
     let hiddenDirsVisible = localStorage.getItem('hiddenDirsVisible');
 
-    if (hiddenDirsVisible === 'true') {
-        icon.title = 'Disable hidden folders';
-        icon.innerText = 'visibility';
+    checkbox.checked = hiddenDirsVisible;
+    setHiddenDirs(hiddenDirsVisible);
+}
+
+function setHiddenDirs(value) {
+    if (value === 'true') {
         directories.forEach((element) => {
             element.classList.remove('hidden');
         });
+    } else if (value === 'false') {
+        directories.forEach((element) => {
+            element.classList.add('hidden');
+        });
     }
-}
+
+    localStorage.setItem('hiddenDirsVisible', value);
+};
