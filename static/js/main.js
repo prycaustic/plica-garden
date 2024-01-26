0d9fc3c9855d030f43cb3d9bf31d9c8f9bc3d073
Andrei Neacsu <andnea823@gmail.com>
2024-01-26 15:08:30 -0600

    Convert settings menu into a <dialog>

--- static/js/main.js
+++ static/js/main.js
@@ -5,18 +5,39 @@ window.addEventListener('load', () => {
     // There's gotta be a better way to build this lol
     menu += '\n<label id="settings-label" class="icon-checkbox" for="settings-menu-toggle"><i class="icon icon-gear dark"></i></label>';
     menu += '\n<input id="settings-menu-toggle" type="checkbox" />';
-    menu += '\n<fieldset id="settings-menu" class="rounded shadow">';
+    menu += '\n<dialog id="settings-menu" class="rounded shadow">';
+    menu += '\n<i class="icon icon-close close-button"></i>';
+    menu += '\n<section id="settings-menu-wrapper">';
     menu += '\n<span class="legend">Settings</span>';
     menu += '\n<label for="show-hidden-dirs">Show hidden folders</label>';
     menu += '\n<input id="show-hidden-dirs" type="checkbox" />';
-    menu += '\n</fieldset>';
+    menu += '\n<label for="show-hidden-files">Show hidden files</label>';
+    menu += '\n<input id="show-hidden-files" type="checkbox" />';
+    menu += '\n</section>';
+    menu += '\n</dialog>';
     footer.innerHTML += menu;
     directories = document.querySelectorAll('.hidden-dir');
-    checkbox = document.querySelector('#show-hidden-dirs');
+    settingsMenu = document.getElementById('settings-menu');
+    closeButton = settingsMenu.querySelector('.icon-close');
 
-    checkbox.addEventListener('change', () => {
+    closeButton.addEventListener('click', () => {
+        settingsMenu.close();
+    });
+
+    settingsToggle = document.getElementById('settings-menu-toggle');
+    settingsToggle.addEventListener('click', (e) => {
+        console.log(settingsMenu.getAttribute('open'));
+        if (settingsMenu.getAttribute('open') == null) {
+            settingsMenu.showModal();
+        } else {
+            settingsMenu.close();
+        }
+    });
+
+    showHidden = document.getElementById('show-hidden-dirs');
+    showHidden.addEventListener('change', () => {
         // fucking javascript LMAO!
-        setHiddenDirs(checkbox.checked.toString());
+        setHiddenDirs(showHidden.checked.toString());
     });
     
     loadHiddenState();
@@ -25,7 +46,7 @@ window.addEventListener('load', () => {
 function loadHiddenState() {
     let hiddenDirsVisible = localStorage.getItem('hiddenDirsVisible');
 
-    checkbox.checked = hiddenDirsVisible;
+    showHidden.checked = hiddenDirsVisible === 'true';
     setHiddenDirs(hiddenDirsVisible);
 }
 
